417b94df4f178b2fd46829817fe51f0b14f374cb
Luke Loera <lgloera2023@gmail.com>
2021-11-10 10:41:15 -0600

    Add Django Heroku configuration package

--- project3_backend/project3_backend/settings.py
+++ project3_backend/project3_backend/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import dj_database_url
 import os
 import psycopg2
+import django_heroku
 import whitenoise
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
