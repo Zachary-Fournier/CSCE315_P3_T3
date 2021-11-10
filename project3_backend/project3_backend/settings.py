c75b13d0ad55895af4a7b6027b4d9d4e802e7a98
Luke Loera <lgloera2023@gmail.com>
2021-11-10 10:42:20 -0600

    Added line for config

--- project3_backend/project3_backend/settings.py
+++ project3_backend/project3_backend/settings.py
@@ -133,3 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Configure for Heroku
+django_heroku.settings(locals())
