6114fa7fda28092e56a517e44008af30aa06c98d
Luke Loera <lgloera2023@gmail.com>
2021-11-17 11:17:54 -0600

    Wrong model

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BaszlAccount, ToDoList
+from .models import BaszlAccount
 # Register your models here.
 
-admin.site.register(ToDoList)
+admin.site.register(BaszlAccount)
