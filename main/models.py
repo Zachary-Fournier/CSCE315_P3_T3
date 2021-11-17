109a8c31aef44370365aeed484036543fc3960a8
Luke Loera <lgloera2023@gmail.com>
2021-11-17 11:15:33 -0600

    Add database models

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cryptography.fernet import Fernet
 
 # Create your models here.
 
@@ -15,3 +16,30 @@ class Item(models.Model):
 
     def __str__(self):
         return self.text
+
+class SocialMediaAccount(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    token = models.CharField(max_length=200)
+    fernet = Fernet(Fernet.generate_key())
+
+    def setPassword(self, __password):
+        self.password = self.fernet.encrypt(__password.encode())
+        return
+
+    def setToken(self, __token):
+        self.token = self.fernet.encrypt(__token.encode())
+        return
+
+    def getPassword(self):
+        return self.fernet.decrypt(self.password).decode()
+
+    def getToken(self):
+        return self.fernet.decrypt(self.token).decode()
+
+class BaszlAccount(models.Model):
+    baszlUser = models.CharField(max_length=200)
+    baszlPass = models.CharField(max_length=200)
+    twitterAccount = models.ForeignKey(SocialMediaAccount, on_delete=models.CASCADE, related_name="twitter")
+    facebookAccount = models.ForeignKey(SocialMediaAccount, on_delete=models.CASCADE, related_name="facebook")
+    instagramAccount = models.ForeignKey(SocialMediaAccount, on_delete=models.CASCADE, related_name="instagram")
