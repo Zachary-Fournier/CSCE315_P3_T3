53fb2c997b4f00facdfb2e86cc71a1dc8a9bc0ce
Luke Loera <lgloera2023@gmail.com>
2021-11-19 16:25:34 -0600

    Base templates

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("<int:nm>", views.test, name="test"),
+    path("", views.home, name="home"),
 ]
--- main/views.py
+++ main/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(response):
-    return HttpResponse("<h1>Howdy world!</h1>")
+def test(response, nm):
+    return render(response, "main/testDynamic.html", {"num":nm})
+
+def home(response):
+    return render(response, "main/home.html", {"name":"Baszl"})
