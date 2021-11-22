16f71c11584c1e41a1f9a214765ba628987a6342
Asim Maredia <amaredia3@gmail.com>
2021-11-21 19:36:34 -0600

    access user token

--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from allauth.socialaccount.models import SocialToken
+
+print("Printing social token...")
+print(SocialToken)
 
 # Create your views here.
 def test(response, nm):
