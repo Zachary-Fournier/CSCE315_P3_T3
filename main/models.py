061bc6d4038c833e02fb292f406ef2d6ac3c2a04
Luke Loera <lgloera2023@gmail.com>
2021-11-30 11:05:07 -0600

    Remove test model

--- main/models.py
+++ main/models.py
@@ -3,20 +3,6 @@ from cryptography.fernet import Fernet
 
 # Create your models here.
 
-class ToDoList(models.Model):
-    name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-    
-class Item(models.Model):
-    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
-    complete = models.BooleanField()
-
-    def __str__(self):
-        return self.text
-
 class SocialMediaAccount(models.Model):
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
