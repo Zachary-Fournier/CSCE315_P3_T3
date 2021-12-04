661ea237a4ccc3ca25ef9d49841f836bc8a9d10c
Luke Loera <lgloera2023@gmail.com>
2021-12-04 12:43:10 -0600

    More reconfiguring

--- static/fbAuth.js
+++ static/fbAuth.js
@@ -14,6 +14,13 @@ function statusChangeCallback(response) {
     }
 }
 
+// Called when a person is finished with the Login Button.
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {   // See the onlogin handler
+      statusChangeCallback(response);
+    });
+}
+
 window.fbAsyncInit = function() {
     FB.init({
       appId      : '628039024905744',
@@ -24,7 +31,18 @@ window.fbAsyncInit = function() {
 
     document.querySelector(".fb-login-button").addEventListener("click", () => {
         FB.login(function(response) {
-            statusChangeCallback(response);
+            if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+                token = window.localStorage.getItem('fblst_628039024905744');
+                if (token) {
+                    console.log('token found');
+                    let redirectURL = "https://baszl.herokuapp.com/fbtoken/" + token;
+                    window.location.replace(redirectURL);
+                } else {
+                    console.log('token not found');
+                }  
+            } else {                                 // Not logged into your webpage or we are unable to tell.
+                console.log("Not logged in.");
+            }
         }, {scope: 'public_profile,email,pages_manage_posts'});
     });
 }
