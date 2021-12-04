28342b7a255c18b409b6bf29733d00eb1c5f2d2b
Luke Loera <lgloera2023@gmail.com>
2021-12-03 20:27:16 -0600

    Meh

--- main/views.py
+++ main/views.py
@@ -82,6 +82,8 @@ def getTwitterAccess(request):
         
         secret = AUTH.access_token_secret
         AUTH.set_access_token(key, secret)
+
+        return HttpResponse("<p>" + key + "</p><p>" + secret + "</p>")
         
         #Save to account
         fernet = Fernet(getKey(request.user.username))
