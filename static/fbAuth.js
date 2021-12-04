a45de9aafe0cf98bcf8efe4db3429d20799aa205
Luke Loera <lgloera2023@gmail.com>
2021-12-04 12:37:57 -0600

    Reconfigure facebook SDK

--- static/fbAuth.js
+++ static/fbAuth.js
@@ -14,23 +14,12 @@ function statusChangeCallback(response) {
     }
 }
 
-// Called when a person is finished with the Login Button.
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {   // See the onlogin handler
-      statusChangeCallback(response);
-    });
-}
-
 window.fbAsyncInit = function() {
     FB.init({
       appId      : '628039024905744',
       cookie     : true,                     // Enable cookies to allow the server to access the session.
       xfbml      : true,                     // Parse social plugins on this webpage.
       version    : 'v12.0'           // Use this Graph API version for this call.
-    });
-
-    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
-      statusChangeCallback(response);        // Returns the login status.
     });
 
     document.querySelector(".fb-login-button").addEventListener("click", () => {
